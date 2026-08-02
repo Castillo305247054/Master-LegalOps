3573076687deb59a2c670aa3e9631dc36d06c45a
Marco Antonio Castillo Frías <305247054@derecho.unam.mx>
2026-08-01 22:56:11 -0600

    Change GitHub Personal Access Token
    Updated GitHub Personal Access Token in configuration.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const GITHUB_CONFIG = {
   owner: "Castillo305247054",          // Tu usuario de GitHub
   repo: "Master-LegalOps",             // Tu repositorio público
   filePath: "master-legal.csv", // 👈 Nombre EXACTO como figura en tu repositorio
-  token: "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" // Tu Personal Access Token (PAT)
+  token: "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" // Tu Personal Access Token (PAT)
 };
 
 // ==========================================
